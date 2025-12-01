e9149ad230f573e521cab7012af53cb0843174e0
amirsina <mandegari.amirsina@gmail.com>
2025-12-01 23:10:02 +0330

    make structure better

--- day1/solution.py
+++ day1/solution.py
@@ -1,5 +1,5 @@
 class Solution():
-    def count_stay_on_zero(self, sequence):
+    def stay_on_zero(self, sequence):
         current = 50
         see_zero = 0
         for step in sequence:
@@ -12,31 +12,27 @@ class Solution():
 
             if current == 0:
                 see_zero +=1
-        print(f"saw zero: {see_zero}")
         return see_zero
 
-    def count_pass_on_zero(self, sequence):
+    def pass_on_zero(self, sequence):
         current = 50
         pass_zero = 0
         for step in sequence:
             direction, num = self._seperate_direction_and_step(step)
-            print(direction, num)
-            print(current)
             moded_num = self._mode_hundred(num)
             if direction == 'L':
                 need_to_see_zero = current
                 if num>=need_to_see_zero:
-                    pass_zero = pass_zero+1 if need_to_see_zero is not 0 else pass_zero
+                    pass_zero = pass_zero+1 if need_to_see_zero != 0 else pass_zero
                     pass_zero += ((num-need_to_see_zero)//100)
                 current = self._mode_hundred(current-moded_num)
             else:
                 need_to_see_zero = 100-current
                 if num>=need_to_see_zero:
-                    pass_zero = pass_zero+1 if need_to_see_zero is not 0 else pass_zero
+                    pass_zero = pass_zero+1 if need_to_see_zero != 0 else pass_zero
                     pass_zero += (num-need_to_see_zero)//100
                 current = self._mode_hundred(current+moded_num)
 
-        print(f"passed zero: {pass_zero}")
         return pass_zero
     
     def _seperate_direction_and_step(self, step):
@@ -46,8 +42,8 @@ class Solution():
         return num % 100
 
 
-
-with open('input', 'r') as file:
-    directions = file.read().splitlines()
-# Now `lines` is a Python list containing each line from the input
-Solution().count_pass_on_zero(directions)
+if __name__ == "__main__":
+    with open('input', 'r') as file:
+        directions = file.read().splitlines()
+    print(f"question one answer is: {Solution().stay_on_zero(directions)}")
+    print(f"question two answer is: {Solution().pass_on_zero(directions)}")
